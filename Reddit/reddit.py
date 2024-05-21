58987f7bff1a7cb268d33d3c846fdba1508eac42
Eason-Wangy366 <754459450@qq.com>
2024-05-21 11:15:42 -0700

    Update reddit

--- Reddit/reddit.py
+++ Reddit/reddit.py
@@ -20,8 +20,8 @@ comment_limit = 1  # number of comment_ for each Reddit Communities
 
 
 def get_data():
-    post_data = pd.DataFrame(columns=['id', 'Community', 'author', 'url', 'timestamp', 'text'])
-    comment_data = pd.DataFrame(columns=['id', 'Community', 'author', 'url', 'timestamp', 'body'])
+    post_data = pd.DataFrame(columns=['id', 'Community', 'author', 'url', 'timestamp', 'text', 'upvote', 'upvote_ratio'])
+    comment_data = pd.DataFrame(columns=['id', 'Community', 'author', 'url', 'timestamp', 'body', 'upvote'])
     for subreddit in subreddits:
 
         try:
@@ -29,12 +29,14 @@ def get_data():
             posts = reddit.subreddit(subreddit)
             for submission in posts.top(limit=Post_limit):
                 new_row = pd.DataFrame({
-                    'id': [submission.id],
-                    'Community': [subreddit],
-                    'author': [''] if submission.author is None else [submission.author.name],
-                    'url': ['https://www.reddit.com' + submission.permalink],
-                    'timestamp': [submission.created_utc],
-                    'text': [submission.selftext]
+                    'id': [submission.id],  # ID of the submission.
+                    'Community': [subreddit], # # subreddit
+                    'author': [''] if submission.author is None else [submission.author.name],  # name of author
+                    'url': ['https://www.reddit.com' + submission.permalink],  # A permalink for the submission.
+                    'timestamp': [submission.created_utc],  # Time the submission was created, represented in `Unix Time`_.
+                    'text': [submission.selftext],  # The submissions' selftext - an empty string if a link post.
+                    'upvote': [submission.score],  # The number of upvotes for the submission.
+                    'upvote_ratio': [submission.upvote_ratio]  # The percentage of upvotes from all votes on the submission.
                 })
                 post_data = pd.concat([post_data, new_row], ignore_index=True)
 
@@ -44,12 +46,13 @@ def get_data():
                 if i >= comment_limit:
                     break
                 new_row = pd.DataFrame({
-                    'id': [comment.id],
-                    'Community': [subreddit],
-                    'author': [''] if comment.author is None else [comment.author.name],
-                    'url': ['https://www.reddit.com' + comment.permalink],
-                    'timestamp': [comment.created_utc],
-                    'body': [comment.body]
+                    'id': [comment.id],  # The ID of the comment.
+                    'Community': [subreddit],  # subreddit
+                    'author': [''] if comment.author is None else [comment.author.name],  # name of author
+                    'url': ['https://www.reddit.com' + comment.permalink],  # A permalink for the comment.
+                    'timestamp': [comment.created_utc],  # Time the comment was created, represented in `Unix Time`_.
+                    'body': [comment.body],  # The body of the comment, as Markdown.
+                    'upvote': [comment.score]  # The number of upvotes for the comment.
                 })
                 comment_data = pd.concat([comment_data, new_row], ignore_index=True)
                 i += 1
